b84fe27debd785610c1a2f3616ee22f261db458d
Dmytro Vasylkiv <67468470+Dima-Vas@users.noreply.github.com>
2022-10-31 15:44:09 +0200

    Add files via upload

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -94,10 +94,14 @@ int main(void)
 
 	    BSP_ACCELERO_GetXYZ(buffer);
 	          if (buffer[2] > 0){
-	            HAL_GPIO_WritePin(GPIOD, GPIO_PIN_12, GPIO_PIN_RESET);
+	        	  HAL_GPIO_WritePin(GPIOD, GPIO_PIN_12, GPIO_PIN_RESET);
+	        	  HAL_GPIO_WritePin(GPIOD, GPIO_PIN_13, GPIO_PIN_RESET);
+				  HAL_GPIO_WritePin(GPIOD, GPIO_PIN_14, GPIO_PIN_RESET);
 	           }
 	          else{
 	            HAL_GPIO_WritePin(GPIOD, GPIO_PIN_12, GPIO_PIN_SET);
+	            HAL_GPIO_WritePin(GPIOD, GPIO_PIN_13, GPIO_PIN_SET);
+	            HAL_GPIO_WritePin(GPIOD, GPIO_PIN_14, GPIO_PIN_SET);
 	          }
 	   HAL_GPIO_WritePin(GPIOD, GPIO_PIN_15, GPIO_PIN_SET);
 	    /* USER CODE BEGIN 3 */
