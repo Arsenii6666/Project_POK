9bf8e5086ca7e1ddbea8af9cdca0f5bc4942b15d
Arsenii6666 <92683180+Arsenii6666@users.noreply.github.com>
2022-10-31 13:22:55 +0200

    init

---
+++ Debug/Drivers/BSP/STM32F411E-Discovery/subdir.mk
@@ -0,0 +1,30 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+# Toolchain: GNU Tools for STM32 (10.3-2021.10)
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../Drivers/BSP/STM32F411E-Discovery/stm32f411e_discovery.c \
+../Drivers/BSP/STM32F411E-Discovery/stm32f411e_discovery_accelerometer.c 
+
+OBJS += \
+./Drivers/BSP/STM32F411E-Discovery/stm32f411e_discovery.o \
+./Drivers/BSP/STM32F411E-Discovery/stm32f411e_discovery_accelerometer.o 
+
+C_DEPS += \
+./Drivers/BSP/STM32F411E-Discovery/stm32f411e_discovery.d \
+./Drivers/BSP/STM32F411E-Discovery/stm32f411e_discovery_accelerometer.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+Drivers/BSP/STM32F411E-Discovery/%.o Drivers/BSP/STM32F411E-Discovery/%.su: ../Drivers/BSP/STM32F411E-Discovery/%.c Drivers/BSP/STM32F411E-Discovery/subdir.mk
+	arm-none-eabi-gcc "$<" -mcpu=cortex-m4 -std=gnu11 -g3 -DDEBUG -DUSE_HAL_DRIVER -DSTM32F411xE -c -I../USB_HOST/App -I../USB_HOST/Target -I../Core/Inc -I../Drivers/STM32F4xx_HAL_Driver/Inc -I../Drivers/STM32F4xx_HAL_Driver/Inc/Legacy -I../Middlewares/ST/STM32_USB_Host_Library/Core/Inc -I../Middlewares/ST/STM32_USB_Host_Library/Class/CDC/Inc -I../Drivers/CMSIS/Device/ST/STM32F4xx/Include -I../Drivers/CMSIS/Include -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" --specs=nano.specs -mfpu=fpv4-sp-d16 -mfloat-abi=hard -mthumb -o "$@"
+
+clean: clean-Drivers-2f-BSP-2f-STM32F411E-2d-Discovery
+
+clean-Drivers-2f-BSP-2f-STM32F411E-2d-Discovery:
+	-$(RM) ./Drivers/BSP/STM32F411E-Discovery/stm32f411e_discovery.d ./Drivers/BSP/STM32F411E-Discovery/stm32f411e_discovery.o ./Drivers/BSP/STM32F411E-Discovery/stm32f411e_discovery.su ./Drivers/BSP/STM32F411E-Discovery/stm32f411e_discovery_accelerometer.d ./Drivers/BSP/STM32F411E-Discovery/stm32f411e_discovery_accelerometer.o ./Drivers/BSP/STM32F411E-Discovery/stm32f411e_discovery_accelerometer.su
+
+.PHONY: clean-Drivers-2f-BSP-2f-STM32F411E-2d-Discovery
+
